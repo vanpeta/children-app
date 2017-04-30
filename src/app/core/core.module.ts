c61a4d25ef8ffebb84fccfc0186bd99e764e49b6
Carlos Allende <carlosallendesanz@gmail.com>
2017-04-30 15:28:39 -0700

    children service created

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,22 @@
 import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
+//components
 import { HeaderComponent } from './header/header.component';
 import { ChildrenComponent } from './children/children.component';
 import { ChildComponent } from './child/child.component';
-
+//services
+import { ChildrenService } from './children-service/children.service';
 
 @NgModule({
   imports: [
-    SharedModule
+    SharedModule,
+    ChildrenService
   ],
-  declarations: [HeaderComponent, ChildrenComponent, ChildComponent]
+  declarations: [
+      HeaderComponent,
+      ChildrenComponent,
+      ChildComponent
+  ],
+  providers: [ ChildrenService ]
 })
 export class CoreModule { }
